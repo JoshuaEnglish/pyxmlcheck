97f25b8ee00fc30c90fd1435e929880f98953365
Joshua.R.English <Joshua.R.English@0ce29d86-bd9f-11de-ba47-45a14fc341d6>
2009-10-20 17:43:06 +0000

    First Upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='XMLCheck',
+      version= '1.0',
+      description='Python validator for xml data files',
+      author="Josh English",
+      author_email="Joshua.R.English@gmail.com",
+      url="http://www.spiritone.com/~english/code/xmlcheck/",
+      packages=['XMLCheck']
+      )
