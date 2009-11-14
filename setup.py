887074c895b2dc1726e661314b3a5288f8f7b8f9
Joshua.R.English <Joshua.R.English@0ce29d86-bd9f-11de-ba47-45a14fc341d6>
2009-11-14 04:25:35 +0000

    added test of the xcheck file plus first draft of documentation

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup
 setup(name='XMLCheck',
-      version= '1.0',
+      version= '1.2.7',
       description='Python validator for xml data files',
       author="Josh English",
       author_email="Joshua.R.English@gmail.com",
-      url="http://www.spiritone.com/~english/code/xmlcheck/",
-      packages=['XMLCheck']
+      url="http://code.google.com/p/pyxmlcheck/",
+      packages=['XMLCheck'],
+      platform = 'Any'
       )
