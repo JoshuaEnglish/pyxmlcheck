596bf04eb8f119b53eb2f70d1d9f858b9b538dab
joshua.r.english@gmail.com <joshua.r.english@gmail.com@0ce29d86-bd9f-11de-ba47-45a14fc341d6>
2013-03-16 03:43:29 +0000

    Test after a long development absence

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='XMLCheck',
-      version= '1.2.7',
+      version= '0.6.5',
       description='Python validator for xml data files',
       author="Josh English",
       author_email="Joshua.R.English@gmail.com",
